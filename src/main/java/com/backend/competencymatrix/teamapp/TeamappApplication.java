1a220a16b357b3a276afc35ab1f555e422f36656
fpawlows <filip.pawlowski001@gmail.com>
2023-08-18 10:05:30 +0200

    Initialized project with spring.io

---
+++ src/main/java/com/backend/competencymatrix/teamapp/TeamappApplication.java
@@ -0,0 +1,13 @@
+package com.backend.competencymatrix.teamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/competencymatrix/teamapp/TeamappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.competencymatrix.teamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
